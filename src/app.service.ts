e235e83414ad98acd71af97fb237252672e6d9e0
nuyeo <bluelayzxx@gmail.com>
2024-09-06 01:11:25 +0900

    fix: Revert changes in app.service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,18 +1,7 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService implements OnModuleInit {
-  constructor(private configService: ConfigService) {}
-
-  onModuleInit() {
-    console.log('DB_HOST:', this.configService.get('DB_HOST'));
-    console.log('DB_PORT:', this.configService.get('DB_PORT'));
-    console.log('DB_USER:', this.configService.get('DB_USER'));
-    console.log('DB_PASSWORD:', this.configService.get('DB_PASSWORD'));
-    console.log('DB_NAME:', this.configService.get('DB_NAME'));
-  }
-
+export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
