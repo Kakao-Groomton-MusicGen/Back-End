4290290c0b3cc8fc58f69ef99136b514bec40ce8
nuyeo <bluelayzxx@gmail.com>
2024-09-06 22:57:22 +0900

    feat: Add posts.service

---
+++ src/posts/posts.service.ts
@@ -0,0 +1,63 @@
+import { Injectable, NotFoundException, UnauthorizedException } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import * as bcrypt from "bcrypt";
+import { Posts } from "../entities/posts.entity";
+import { CreatePostDto } from "./dtos/create-post.dto";
+import { UpdatePostDto } from "./dtos/update-post.dto";
+
+@Injectable()
+export class PostsService {
+    constructor(
+        @InjectRepository(Posts)
+        private postsRepository: Repository<Posts>,
+    ) {}
+
+    async createPost(createPostDto: CreatePostDto): Promise<Posts> {
+        const { password, ...rest } = createPostDto;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const post = this.postsRepository.create({
+            ...rest,
+            password: hashedPassword,
+        });
+
+        return this.postsRepository.save(post);
+    }
+
+    async updatePost(id: number, updatePostDto: UpdatePostDto): Promise<Posts> {
+        const post = await this.getPostById(id);
+
+        if (!await bcrypt.compare(updatePostDto.password, post.password)) {
+            throw new UnauthorizedException('Invalid password');
+          }
+
+          const { password, ...rest } = updatePostDto;
+          Object.assign(post, rest);
+
+        return this.postsRepository.save(post);
+    }
+
+    deletePost = async (id: number, password: string): Promise<void> => {
+        const post = await this.getPostById(id);
+
+        if (!await bcrypt.compare(password, post.password)) {
+            throw new UnauthorizedException('Invalid password');
+        }
+
+        await this.postsRepository.remove(post);
+    }
+
+    async getPostById(id: number): Promise<Posts> {
+        const post = await this.postsRepository.findOne({ where: { id } });
+
+        if (!post) {
+            throw new NotFoundException(`Post with id: ${id} not found`);
+        }
+
+        return post;
+    }
+
+    async getAllPosts(): Promise<Posts[]> {
+        return this.postsRepository.find();
+    }
+}
