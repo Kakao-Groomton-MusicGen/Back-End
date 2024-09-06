16da8069cc521c6ded3d540b20aaf44d149a6288
nuyeo <bluelayzxx@gmail.com>
2024-09-06 22:22:28 +0900

    feat: Add update-post DTO

---
+++ src/posts/dtos/update-post.dto.ts
@@ -0,0 +1,18 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString, MinLength } from "class-validator";
+
+export class UpdatePostDto {
+    @ApiProperty({ description: '수정할 게시글 제목', required: false })
+    @IsString()
+    title?: string;
+  
+    @ApiProperty({ description: '수정할 게시글 내용', required: false })
+    @IsString()
+    content?: string;
+  
+    @ApiProperty({ description: '게시글 비밀번호', minLength: 6 })
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(6)
+    password: string;
+}
