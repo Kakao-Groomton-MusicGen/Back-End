88a4a595d7ddcf4762ba29e35d9f596359cb0686
nuyeo <bluelayzxx@gmail.com>
2024-09-07 01:17:24 +0900

    refactor: Modify updatePost to return PostResponseDto

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -5,6 +5,7 @@ import * as bcrypt from "bcrypt";
 import { Posts } from "../entities/posts.entity";
 import { CreatePostDto } from "./dtos/create-post.dto";
 import { UpdatePostDto } from "./dtos/update-post.dto";
+import { PostResponseDto } from "./dtos/post-response.dto";
 
 @Injectable()
 export class PostsService {
@@ -13,7 +14,7 @@ export class PostsService {
         private postsRepository: Repository<Posts>,
     ) {}
 
-    async createPost(createPostDto: CreatePostDto): Promise<Posts> {
+    async createPost(createPostDto: CreatePostDto): Promise<PostResponseDto> {
         const { password, ...rest } = createPostDto;
         const hashedPassword = await bcrypt.hash(password, 10);
         const post = this.postsRepository.create({
@@ -25,11 +26,15 @@ export class PostsService {
     }
 
     async updatePost(id: number, updatePostDto: UpdatePostDto): Promise<Posts> {
-        const post = await this.getPostById(id);
+        const post = await this.postsRepository.findOne({ where: { id } });
+
+        if (!post) {
+            throw new NotFoundException(`Post with id: ${id} not found`);
+        }
 
         if (!await bcrypt.compare(updatePostDto.password, post.password)) {
             throw new UnauthorizedException('Invalid password');
-          }
+        }
 
           const { password, ...rest } = updatePostDto;
           Object.assign(post, rest);
@@ -38,7 +43,11 @@ export class PostsService {
     }
 
     deletePost = async (id: number, password: string): Promise<void> => {
-        const post = await this.getPostById(id);
+        const post = await this.postsRepository.findOne({ where: { id } });
+
+        if (!post) {
+            throw new NotFoundException(`Post with id: ${id} not found`);
+        }
 
         if (!await bcrypt.compare(password, post.password)) {
             throw new UnauthorizedException('Invalid password');
@@ -47,17 +56,29 @@ export class PostsService {
         await this.postsRepository.remove(post);
     }
 
-    async getPostById(id: number): Promise<Posts> {
+    async getPostById(id: number): Promise<PostResponseDto> {
         const post = await this.postsRepository.findOne({ where: { id } });
 
         if (!post) {
             throw new NotFoundException(`Post with id: ${id} not found`);
         }
 
-        return post;
+        return this.toResponseDto(post);
     }
 
     async getAllPosts(): Promise<Posts[]> {
         return this.postsRepository.find();
     }
+
+    private toResponseDto(post: Posts): PostResponseDto {
+        return {
+          id: post.id,
+          title: post.title,
+          contents: post.contents,
+          user: post.user,
+          song: { id: post.song.id },
+          created_at: post.created_at,
+          updated_at: post.updated_at,
+        };
+      }
 }
