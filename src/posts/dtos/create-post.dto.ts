82dd0820a4130ce51dc7ee83e0f34068d7147297
nuyeo <bluelayzxx@gmail.com>
2024-09-06 22:12:55 +0900

    feat: Add create-post DTO

---
+++ src/posts/dtos/create-post.dto.ts
@@ -0,0 +1,30 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString, IsNumber, MinLength } from "class-validator";
+
+export class CreatePostDto {
+    @ApiProperty({ description: '게시글 제목' })
+    @IsNotEmpty({ message: '게시글 제목을 입력해주세요.' })
+    @IsString()
+    title: string;
+  
+    @ApiProperty({ description: '게시글 내용' })
+    @IsNotEmpty({ message: '게시글 내용을 입력해주세요.' })
+    @IsString()
+    content: string;
+
+    @ApiProperty({ description: '게시글 작성자' })
+    @IsNotEmpty({ message: '게시글 작성자를 입력해주세요.' })
+    @IsString()
+    user: string;
+
+    @ApiProperty({ description: '게시글 비밀번호', minLength: 6 })
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(6, { message: '비밀번호는 6자 이상이어야 합니다.' })
+    password: string;
+
+    @ApiProperty({ description: '만든 음악 ID' })
+    @IsNotEmpty()
+    @IsNumber()
+    songId: number;
+}
