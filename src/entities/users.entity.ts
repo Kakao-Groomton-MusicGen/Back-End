1872a27899c22710923bd9e3ed7fb595109f408d
nuyeo <bluelayzxx@gmail.com>
2024-09-06 21:38:35 +0900

    refactor: Modify columns in Users entity

--- src/entities/users.entity.ts
+++ src/entities/users.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, CreateDateColumn, UpdateDateColumn, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Users {
@@ -7,4 +7,10 @@ export class Users {
 
     @Column()
     password: string;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
 }
