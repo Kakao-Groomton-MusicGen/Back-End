699c7eecb7918e8f1d9509921ace7a11120bb478
nuyeo <bluelayzxx@gmail.com>
2024-09-06 13:05:16 +0900

    feat: Add songs.module

---
+++ src/songs/songs.module.ts
@@ -0,0 +1,14 @@
+// src/songs/songs.module.ts
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { HttpModule } from '@nestjs/axios';
+import { SongsController } from './songs.controller';
+import { SongsService } from './songs.service';
+import { Songs } from '../entities/songs.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Songs]), HttpModule],
+  controllers: [SongsController],
+  providers: [SongsService],
+})
+export class SongsModule {}
